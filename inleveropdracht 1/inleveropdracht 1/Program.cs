64b450575c44acd30c326d4604df329b2f9132ae
vinson321 <94622300+vinson321@users.noreply.github.com>
2021-12-05 21:42:52 +0100

    Update Program.cs

--- inleveropdracht 1/inleveropdracht 1/Program.cs
+++ inleveropdracht 1/inleveropdracht 1/Program.cs
@@ -10,7 +10,7 @@ namespace inleveropdracht_1
     {
         static void Main(string[] args)
         {
-         int v = 1;
+            int v = 1;
             int w = 2;
             int q = 3;
             int s = 4;
@@ -18,7 +18,21 @@ namespace inleveropdracht_1
             string naam2;
             string naam3;
             string naam4;
+            int inzet1;
+            int inzet2;
+            int inzet3;
+            int inzet4;
+            int spelers;
+            int kaarten;
+            string keuze;
+            int dubbel;
 
+            Console.WriteLine("aantal spelers:");
+            spelers = Int32.Parse(Console.ReadLine());
+           
+            Console.WriteLine("aantal kaarten per spelers:");
+            kaarten = Int32.Parse(Console.ReadLine());
+           
             naam1 = string.Format(Console.ReadLine());
             naam2 = string.Format(Console.ReadLine());
             naam3 = string.Format(Console.ReadLine());
@@ -68,6 +82,99 @@ namespace inleveropdracht_1
                 Console.WriteLine("naam speler 4:");
                 naam4 = string.Format(Console.ReadLine());
             }
+            
+            
+            
+            if (spelers == v)
+            {
+                Console.WriteLine("inzet speler 1:");
+                inzet1 = Int32.Parse(Console.ReadLine());
+                Console.WriteLine(naam1);
+                Console.WriteLine("kaart:");
+                Console.Write("inzet speler 1: " + inzet1);
+
+
+            }
+
+
+            else if(spelers == w)
+            {
+                Console.WriteLine("inzet speler 1:");
+                inzet1 = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("inzet speler 2:");
+                inzet2 = Int32.Parse(Console.ReadLine());
+                Console.WriteLine(naam1);
+                Console.WriteLine("kaart:");
+                Console.WriteLine(value: "inzet speler 1: " + inzet1);
+                Console.WriteLine(naam2);
+                Console.WriteLine("kaart:");
+                Console.Write("inzet speler 2: " + inzet2);
+
+            }
+           
+            if (spelers == q)
+            {
+                Console.WriteLine("inzet speler 1:");
+                inzet1 = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("inzet speler 2:");
+                inzet2 = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("inzet speler 3:");
+                inzet3 = Int32.Parse(Console.ReadLine());
+                Console.WriteLine(naam1);
+                Console.WriteLine("kaart:");
+                Console.WriteLine(value: "inzet speler 1: " + inzet1);
+                Console.WriteLine(naam2);
+                Console.WriteLine("kaart:");
+                Console.WriteLine(value: "inzet speler 2: " + inzet2);
+                Console.WriteLine(naam3);
+                Console.WriteLine("kaart:");
+                Console.WriteLine(value: "inzet speler 3: " + inzet3);
+
+            }
+
+            if (spelers == s)
+            {
+                Console.WriteLine("inzet speler 1:");
+                inzet1 = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("inzet speler 2:");
+                inzet2 = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("inzet speler 3:");
+                inzet3 = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("inzet speler 4:");
+                inzet4 = Int32.Parse(Console.ReadLine());
+                Console.WriteLine(naam1);
+                Console.WriteLine("kaart:");
+                Console.WriteLine(value: "inzet speler 1: " + inzet1);
+                Console.WriteLine("inzet verdubbelen of niet");
+                keuze = Console.ReadLine().ToUpper();
+                if (keuze == "verdubbelen")
+                {
+                    Console.WriteLine(inzet1 * 2);
+                    dubbel = Int32.Parse(Console.ReadLine());
+                    Console.WriteLine(dubbel);
+
+                }
+                
+                Console.WriteLine(naam2);
+                Console.WriteLine("kaart:");
+                Console.WriteLine(value: "inzet speler 2: " + inzet2);
+                Console.WriteLine(naam3);
+                Console.WriteLine("kaart:");
+                Console.WriteLine(value: "inzet speler 3: " + inzet3);
+                Console.WriteLine(naam4);
+                Console.WriteLine("kaart:");
+                Console.WriteLine(value: "inzet speler 4: " + inzet4);
+
+            }
+            
+            
+            
+            
+            
+            
+            
+            
+            
         }
     }
 }
