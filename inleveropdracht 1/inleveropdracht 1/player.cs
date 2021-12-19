c36cb943fe8adb6c233732d0023ca731ad71c1fb
vinson321 <94622300+vinson321@users.noreply.github.com>
2021-12-19 19:49:13 +0100

    Update player.cs

--- inleveropdracht 1/inleveropdracht 1/player.cs
+++ inleveropdracht 1/inleveropdracht 1/player.cs
@@ -9,7 +9,6 @@ namespace inleveropdracht_1
     class Player
     {
         public string name;
-        public int value;
         protected List<Card> Kaarten = new List<Card>();
 
         public void TakeCard(List<Card> deck, bool isVisible)
